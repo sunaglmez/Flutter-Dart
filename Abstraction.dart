21089b819ec4e53ace9a32f3ead94300afe10f45
Suna Gülmez <109751575+sunaglmez@users.noreply.github.com>
2024-10-02 20:32:00 +0300

    Create Abstraction.dart

---
+++ Abstraction.dart
@@ -0,0 +1,28 @@
+"""
+Abstract Classes and Methods in Dart
+Abstract Class
+Note: You cannot define abstract methods without using the abstract keyword.
+"""
+
+// Abstract Class Definition
+abstract class Vehicle { // abstract class
+ void start();  // abstract method. Since the method's functionality is not defined in the abstract class, it's an abstract method. However, in a concrete subclass like Car, this method will be defined, making it a concrete method there.
+ void stop();   // abstract method
+ void provideInfo() { // non-abstract method
+   print('This is a vehicle.');
+  }
+}
+
+// Concrete Class Derived from Abstract Class
+class Car extends Vehicle {
+  @override
+  void start() {
+    print('Car started.');
+  }
+  
+  @override
+  void stop() {
+    print('Car stopped.');
+  }
+}
+
